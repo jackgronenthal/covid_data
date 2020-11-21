daf487c499ad24ef7fc0dda0919e09d7891c5ce0
Jack Gronenthal <jlg6@illinois.edu>
2020-11-20 18:34:17 -0600

    Deploy ready for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 require('dotenv').config();
 const app = require("./app");
 
-const port = process.env.port || 8888;
+const port = process.env.PORT;
 
 const server = http.createServer(app);
 
